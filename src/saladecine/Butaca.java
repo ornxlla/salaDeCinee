338c1827608888a76a803fad842710213e03a95c
Ornella Alonso Reyes <ornxalonso@gmail.com>
2023-04-07 11:46:35 -0300

    Se actualizo Butaca

--- src/saladecine/Butaca.java
+++ src/saladecine/Butaca.java
@@ -1,12 +1,14 @@
 package saladecine;
 
+//atributos
 public class Butaca {
  private Boolean ocupada;
  
+ // constructor
  public Butaca() {
 	 this.ocupada = false;
  }
- 
+ // metodos
  public Boolean getOcupada() {
 	 return ocupada;
  }
